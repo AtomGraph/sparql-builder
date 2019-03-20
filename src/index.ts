b44946d078bc3d711af9d7864fc67040680f1d76
Martynas <martynas@atomgraph.com>
2019-03-20 01:11:42 +0100

    Webpack configuration

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Type definitions for QueryBuilder 0.0.1
+// Project: AtomGraph Platform
+// Definitions by: Martynas Jusevičius <martynas@atomgraph.com>
+
+export { QueryBuilder } from './com/atomgraph/platform/query/QueryBuilder';
+export { SelectBuilder } from './com/atomgraph/platform/query/SelectBuilder';
